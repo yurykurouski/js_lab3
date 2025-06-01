2e1a7824837d029a3e1329adf2269d428a67322b
Yury Kurouski <ymorpheus@gmail.com>
2025-06-02 00:03:42 +0200

    fix: update ESLint rule for no-unsafe-assignment to warning level

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
   ],
   overrides: [],
   rules: {
+    '@typescript-eslint/no-unsafe-assignment': 1,
     "@typescript-eslint/unbound-method": 0,
     '@typescript-eslint/require-await': 0,
     '@typescript-eslint/ban-ts-comment': 0,
