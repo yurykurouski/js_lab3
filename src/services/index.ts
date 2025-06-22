8170d736badd7e008df9471f1c0f3a39764ffd41
Yury Kurouski <ymorpheus@gmail.com>
2025-06-22 13:38:11 +0200

    refactor: remove PAYMENT_PROCESSED event and PaymentService for simplification

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export { RoomService } from './RoomService';
-export { PaymentService } from './PaymentService';
 export { NotificationService } from './NotificationService';
 export { BookingService } from './BookingService';
 export { HttpService } from './HttpService';
